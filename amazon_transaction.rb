daf9848aa547d090a5cd43bda9b5ab867d28f0bb
merryshen <yihuaca@gmail.com>
2018-07-16 13:49:21 +0000

    Done.

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,6 @@
-# Code your cash register here!
+def initialize(total, amount=0)
+  
+ describe '::new' do
+    it 'sets an instance variable for @total on initialization to zero' do
+      expect(amazon_transaction.instance_variable_get(:@total)).to eq(0)
+    end
